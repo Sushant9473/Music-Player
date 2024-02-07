20886f187203ca9c5aa5cb17946dcd2a5cd49eb4
Sushant <sushatsinghchauhan08@gmail.com>
2024-02-07 20:39:09 +0530

    Music Player Completed

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "img1",
+    src: "Uska Hi Banana 192",
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "img2",
+    src: "Ikson Any",
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "img3",
+    src: "BEAUZ & JVNA Crazy",
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "img2",
+    src: "Ikson Any",
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "img1",
+    src: "Uska Hi Banana 192",
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "img3",
+    src: "BEAUZ & JVNA Crazy",
+  },
+];
